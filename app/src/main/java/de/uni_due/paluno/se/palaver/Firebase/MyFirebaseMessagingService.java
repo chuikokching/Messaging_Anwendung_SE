aa71bf1c1677882554634b932df97f4200bf2e94
chuikokching <guozheng.xu@stud.uni-due.de>
2019-06-18 14:40:28 +0200

    Update.

--- app/src/main/java/de/uni_due/paluno/se/palaver/Firebase/MyFirebaseMessagingService.java
+++ app/src/main/java/de/uni_due/paluno/se/palaver/Firebase/MyFirebaseMessagingService.java
@@ -1,9 +1,14 @@
 package de.uni_due.paluno.se.palaver.Firebase;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
@@ -12,7 +17,10 @@ import com.google.firebase.messaging.RemoteMessage;
 import java.util.Map;
 
 import de.uni_due.paluno.se.palaver.MainActivity;
-import de.uni_due.paluno.se.palaver.Notificationhelper.NotificationHelper;
+
+import de.uni_due.paluno.se.palaver.R;
+
+import static android.support.v4.content.ContextCompat.getSystemService;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
@@ -23,18 +31,45 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         //broadcast
         super.onMessageReceived(message);
 
-
-        if(message.getNotification()!=null)
-        {
-            String title = message.getNotification().getTitle();
-            String text = message.getNotification().getBody();
-            Log.i("tag","test = title :"+title);
-            Log.i("tag","test = text :"+text);
+        //String title = message.getNotification().getTitle();
+        String sender = message.getData().get("sender");
+        String preview = message.getData().get("preview");
+        // Log.i("tag","test = title :"+sender);
+        //Log.i("tag","test = text :"+preview);
 
 
-            NotificationHelper.displayNotification(getApplicationContext(),"Text from palaver",message.getData().toString());
+        createNotificationChannel();
+
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "id")
+                .setSmallIcon(R.drawable.star)
+                .setContentTitle("Text from palaver")
+                .setContentText(sender+" : "+preview)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManager manager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(1,builder.build());
+
+
+    }
+
+        private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String name = "Channel";
+            String description = "Description";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("id", name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
         }
 
-        //sendBroadcast(i);
     }
+
+        //sendBroadcast(i);
+
 }
